6a665de6bbc2b50fb4172308ed340feed81dfd1b
caitlinbrinkley <80763632+caitlinbrinkley@users.noreply.github.com>
2021-04-24 16:06:47 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ var dc = {};
 
 var homeHtml = "snippets/home-snippet.html";
 var allCategoriesUrl =
-  "https://davids-restaurant.herokuapp.com/categories.json";
+  "https://api.jsonbin.io/b/608425ba48f71c7a71cd3b8d";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
